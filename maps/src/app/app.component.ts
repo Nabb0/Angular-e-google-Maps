f9d4d8e0aebb68c7fede6a1362fed54dba78002e
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2023-05-08 09:44:07 +0000

    Aggiungere geojson a google maps 02.2

--- maps/src/app/app.component.ts
+++ maps/src/app/app.component.ts
@@ -11,29 +11,64 @@ export class AppComponent {
   position: any;
   label: string;
   circleOptions: { fillColor: string };
-
+  markerOptions!: google.maps.MarkerOptions;
+  vertices: google.maps.LatLngLiteral[];
+  triangleOption:any ;
   constructor() {
     this.center = { lat: 45.506738, lng: 9.190766 };
     this.position = this.center;
     this.label = "ciao";
     this.circleOptions = { fillColor: 'red' };
+    this.triangleOption = { fillColor: 'red' };
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+      ];
+    
+    let iconData: google.maps.Icon = {
+      url: './assets/img/cat_acrobat.ico',
+      scaledSize: new google.maps.Size(60, 60)
+    }
+    this.markerOptions = { icon: iconData };
+
+
+
   }
 
   changeColor(color: string): void {
     switch (color) {
       case 'red':
-        this.circleOptions.fillColor = 'red';
+        this.circleOptions = { fillColor: 'red' };
         break;
       case 'yellow':
-        this.circleOptions.fillColor = 'yellow';
+        this.circleOptions = { fillColor: 'yellow' };
         break;
       case 'green':
-        this.circleOptions.fillColor = 'green';
+        this.circleOptions = { fillColor: 'green' };
         break;
       default:
-        this.circleOptions.fillColor = 'red';
+        this.circleOptions = { fillColor: 'red' };
         break;
     }
     console.log(this.circleOptions);
   }
+  changeRectangleColor(color: string): void {
+    switch (color) {
+      case 'red':
+        this.triangleOption = { fillColor : 'red' };
+        break;
+      case 'yellow':
+        this.triangleOption = {  fillColor : 'yellow' };
+        break;
+      case 'green':
+        this.triangleOption = { fillColor : 'green' };
+        break;
+      default:
+        this.triangleOption = {  fillColor : 'blue' };
+        break;
+    }
+    console.log('Rectangle color changed to:', color);
   }
+  
+}
