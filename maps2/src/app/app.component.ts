cd5adf1b147590fb4126525dfdde29e5d47ba363
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2023-05-11 12:58:53 +0000

    Spiegone del decoratore @ViewChild 02.2

--- maps2/src/app/app.component.ts
+++ maps2/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps'
+import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'maps2';
+export class AppComponent implements AfterViewInit {
+  title = 'server mappe';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
+  geoJsonObject : GeoFeatureCollection;
+  fillColor: string = "#FF0000";  //Colore delle zone catastali
+ 
+  markerList : google.maps.MarkerOptions[] =[];
+  zoom = 8;
+
+  constructor() {
+   
+
+    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file     gojson.model.ts
+    this.geoJsonObject = GEOJSON;  
+    console.log(this.geoJsonObject.features[0].geometry.coordinates[0][0]); //stampo l'oggetto geoJsonObject sulla console
+  }
+  @ViewChild('mapRef') mapRef!: GoogleMap;
+  ngAfterViewInit() {
+  this.mapRef.data.addGeoJson(this.geoJsonObject);
+  }
+  
 }
