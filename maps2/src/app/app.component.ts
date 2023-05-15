71e8d905f036eecd7db265f4688369f58b7ab8a3
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2023-05-15 09:08:07 +0000

    inizio ReLab - Backend

--- maps2/src/app/app.component.ts
+++ maps2/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements AfterViewInit {
   fillColor: string = "#FF0000";  //Colore delle zone catastali
  
   markerList : google.maps.MarkerOptions[] =[];
-  zoom = 8;
+  zoom = 12;
 
   constructor() {
    
@@ -24,10 +24,49 @@ export class AppComponent implements AfterViewInit {
     //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file     gojson.model.ts
     this.geoJsonObject = GEOJSON;  
     console.log(this.geoJsonObject.features[0].geometry.coordinates[0][0]); //stampo l'oggetto geoJsonObject sulla console
+    this.markerGenerator()
   }
+ 
+
   @ViewChild('mapRef') mapRef!: GoogleMap;
   ngAfterViewInit() {
-  this.mapRef.data.addGeoJson(this.geoJsonObject);
+    this.mapRef.data.addGeoJson(this.geoJsonObject);
+    this.mapRef.data.setStyle(this.styleFunc)
   }
+
+  styleFunc = (feature:any) =>{
+    console.log(feature.h.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.h.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+  markerGenerator()
+  {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
+  }
+
+
+
   
 }
