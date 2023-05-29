770ae0c0f00249537326d1d9e5ed85e8702c0958
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2023-05-29 09:20:49 +0000

    fine ReLab - Backend

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+import geojson
+import shapely.wkt
 
 app = Flask(__name__)
 
@@ -27,10 +29,130 @@ def get_all_addresses():
     mil4326WKT = mongo.db.MilWKT4326
     output = []
     for s in mil4326WKT.find().limit(100):
-        output.append(s['INDIRIZZO'])
+        output.append(s['INDIRIZZO']),
+        output.append(s[('CI_VETTORE')])
     return jsonify({'result': output})
 
-# Checks to see if the name of the package is the run as the main package.
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore(foglio):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "FOGLIO" : int(foglio)
+    }
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE']
+        }
+        )
+    return jsonify(output) #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
+
+#@app.route('/ci_vettore/<sez>', methods=['GET']) Funziona
+def get_sez(sez):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "SEZ" : int(sez)
+    }
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE']
+        }
+        )
+    return jsonify(output) #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
+
+@app.route('/geogeom', methods=['GET'])
+def get_all_stars():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    match = {
+        '$match': {
+            'EP_H_ND': {'$gt': 0}
+        }
+    }
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+    limit = {
+        '$limit' : 10
+    }
+    
+    for s in mil4326WKT.aggregate([match, group, limit]):
+        g1= shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1, 
+        properties={'id':s['_id']['SEZ'], 'media':s['AVG'], 'somma':s['SUM'], 'sezione':s['_id']['SEZ']}) 
+        output.append(g2)                 
+    return jsonify({'result': output})
+
+@app.route('/geogeom/<float:lng>/<float:lat>/<float:r>', methods=['GET'])
+def get_avg(lng, lat, r):
+    print(type(lng))
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    match = {
+        '$match': {
+            '$and':
+            [
+                {'EP_H_ND': {'$gt': 0}},
+                {'WGS84_X': {'$gt': lng - r}},
+                {'WGS84_X': {'$lt': lng + r}},
+                {'WGS84_Y': {'$gt': lat - r}},
+                {'WGS84_Y': {'$lt': lat + r}}
+            ]
+        }
+    }
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+    limit = {
+        '$limit': 10
+    }
+
+    for s in mil4326WKT.aggregate([match, group, limit]):
+        # Converte da WKT in GeoJson Geometry
+        g1 = shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1,
+                             properties={'id': s['_id']['SEZ'], 'media': s['AVG'], 'somma': s['SUM'], 'sezione': s['_id']['SEZ']})
+        output.append(g2)
+    return jsonify(geojson.FeatureCollection(output))
+
+
 if __name__ == "__main__":
-    # Runs the Flask application only if the main.py file is being run.
+    
     app.run()
